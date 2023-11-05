a5ab5d0630b8985a30bde954f1c08f50ecbc7d5f
jeeyo <jeeyo@outlook.com>
2023-11-06 01:26:58 +0700

    add layer to knob for better media control

--- keyboards/sofle/keymaps/npsr/keymap.c
+++ keyboards/sofle/keymaps/npsr/keymap.c
@@ -3,7 +3,8 @@
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD),           ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) }
+    [0] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD),           ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) },
+    [1] = { ENCODER_CCW_CW(KC_MNXT, KC_MPRV),           ENCODER_CCW_CW(KC_DOWN, KC_UP) }
 };
 #endif
 
@@ -19,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| MUTE  |    |  PLAY |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | Del  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LCTR | LGUI | LAlt |      | /Space  /       \Enter \  |  =   |  -   |   [  |   ]  |
+ *            | LCTR | LGUI | LAlt | MO(1)| /Space  /       \Enter \  |  =   |  -   |   [  |   ]  |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -29,6 +30,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS,
   KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,   KC_MPLY, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_DEL,
-                 KC_LCTL,KC_LGUI,KC_LALT, KC_NO, KC_SPC,               KC_ENT, KC_EQL, KC_MINS, KC_LBRC, KC_RBRC
+                 KC_LCTL,KC_LGUI,KC_LALT, MO(1), KC_SPC,               KC_ENT, KC_EQL, KC_MINS, KC_LBRC, KC_RBRC
+),
+
+[1] = LAYOUT(
+  _______,  _______,  _______,  _______,  _______,  _______,                        _______,  _______,  _______,  _______,  _______,  _______,
+  _______,  _______,  _______,  _______,  _______,  _______,                        _______,  _______,  _______,  _______,  _______,  _______,
+  _______,  _______,  _______,  _______,  _______,  _______,                        _______,  _______,  _______,  _______,  _______,  _______,
+  _______,  _______,  _______,  _______,  _______,  _______, _______,      _______, _______,  _______,  _______,  _______,  _______,  _______,
+                 _______,_______,_______, _______, _______,               _______, _______, _______, _______, _______
 )
 };
